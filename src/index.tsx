0309cc9c66dfa27681f41845ae209230db7c1a50
潘博 <1350210100@qq.com>
2020-06-27 22:00:37 +0800

    travis again again

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,6 @@ import './index.css'
 import App from 'containers/App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter as Router } from 'react-router-dom'
-console.log('111')
-console.log('111')
-console.log('111')
-console.log('111')
 
 ReactDOM.render(
 	<React.StrictMode>
