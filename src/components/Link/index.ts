e9a48ca699c5b9ec65c23601a39664f5dcbb456c
潘博 <1350210100@qq.com>
2020-06-28 13:35:46 +0800

    Add: Link

---
+++ src/components/Link/index.ts
@@ -0,0 +1,9 @@
+import { styled } from '@material-ui/styles'
+import { Link as RouteLink } from 'react-router-dom'
+
+const Link = styled(RouteLink)({
+	textDecoration: 'none',
+	color: '#303133'
+})
+
+export default Link
